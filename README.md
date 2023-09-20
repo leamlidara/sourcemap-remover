bfbf5505ac560dde14ef02f90ffb2794dbf0f1ca
Leam Lidara <leamlidara@gmail.com>
2023-09-21 00:12:21 +0700

    Add log + new feature - Fix Service Worker file - Log for file deleted, file fixed, and service worker - Add more support file types

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 A sample command-line application which allow you to remove \*.map files from your WebApp project.
 
+## Why should you remove SourceMap files?
+
+1. **Security**: SourceMap files contain a map of your original source code to the compiled code that is served to users. This can be a security risk, as it could allow attackers to reverse engineer your code and find vulnerabilities.
+2. **Performance**: SourceMap files can increase the size of your production bundle, and can also slow down loading times.
+3. **Privacy**: SourceMap files can contain sensitive information about your codebase, such as internal APIs and implementation details. You may not want to expose this information to users.
+
 ## How to build
 
 ```bash
@@ -18,8 +24,9 @@ OR you can download from below
 
 ```bash
 sourcemap-remover [option [directory path]]
+sourcemap-remover.exe [option [directory path]]
 ```
 
 ### Note
 
-If directory path did not specific, application will get current directory.
+If a directory path is not specified, the application will use the current directory.
--- bin/sourcemap-remover.dart
+++ bin/sourcemap-remover.dart
@@ -1,10 +1,14 @@
+import "dart:convert";
 import "dart:io";
 
 void main(List<String> arguments) {
+  List<String> knowFileTypes = [".js", ".ts", ".css", ".sass"];
   if (arguments.contains("-h") || arguments.contains("--help")) {
     print("sourcemap-remover [option [directory path]]");
     print("options: -h,  --help : display this help");
-    print("If directory path did not specific, application will get current directory.");
+    print("If a directory path is not specified, the application will use the current directory.");
+    print("");
+    print("Support file types: ${knowFileTypes.join(', ')}");
     return;
   }
 
@@ -21,32 +25,39 @@ void main(List<String> arguments) {
     lst = Directory.current.listSync(recursive: true);
   }
 
-  List<String> fileTypes = [".js", ".css"];
   String path = "";
   String fType = "";
   int cnt = lst.length;
-  int cntFileTypes = fileTypes.length;
+  int cntFileTypes = knowFileTypes.length;
   for (var i = 0; i < cnt; i++) {
     if (lst[i] is Directory) continue;
     path = lst[i].path;
 
     //Delete sourceMap + fix sourceMap not found
     for (var j = 0; j < cntFileTypes; j++) {
-      fType = fileTypes[j];
+      fType = knowFileTypes[j];
       try {
-        if (path.endsWith("$fType.map")) File(path).deleteSync();
+        if (path.endsWith("$fType.map")) {
+          try {
+            File(path).deleteSync();
+            print("Deleted: $path");
+          } catch (e) {}
+          continue;
+        }
         if (path.endsWith(fType)) _fixSourceMapNotFound(path);
       } catch (e) {
         print(e);
         return;
       }
     }
+
+    if (path.endsWith("service-worker.js")) _fixServiceWorker(path, knowFileTypes);
   }
 }
 
 void _fixSourceMapNotFound(String path) {
   var f = File(path);
-  var content = f.readAsStringSync();
+  String content = f.readAsStringSync();
   if (content.contains("# sourceMappingURL=") == false) return;
 
   for (var i in [
@@ -56,5 +67,85 @@ void _fixSourceMapNotFound(String path) {
     content = content.replaceAll(i, "");
   }
 
-  f.writeAsString(content.trim());
+  f.writeAsStringSync(content.trim());
+  print("Fixed: $path");
+}
+
+void _fixServiceWorker(String path, List<String> knowFileTypes) {
+  var f = File(path);
+  RegExp regex = RegExp(r"\[(.)+?\]", dotAll: true, multiLine: true, unicode: true);
+  String content = f.readAsStringSync();
+  List<RegExpMatch> matches = regex.allMatches(content).toList();
+  var cnt = matches.length;
+  if (cnt < 1) return;
+  print("Service Worker Found: $path");
+  print("Fixing...");
+
+  bool hasRemove = false;
+  int cntFileType = knowFileTypes.length;
+  for (var i = 0; i < cnt; i++) {
+    List<dynamic> js;
+    String str = matches[i].group(0) ?? "[]";
+    bool isJsonNeedFix = false;
+    try {
+      str = str.replaceAll(RegExp(r"^([\s\t])*", multiLine: true, unicode: true), "");
+      str = str.replaceAllMapped(
+          RegExp(
+            r"([\{\,])([\s\t]+)?([a-zA-Z0-9]+)?([\s\t]+)?:",
+            multiLine: true,
+          ), (m) {
+        isJsonNeedFix = true;
+        return '${m.group(1)}"${m.group(3)}":';
+      });
+      str = str.replaceAll(RegExp(r"\},(\n|\r\n)?\]"), "}]");
+      js = json.decode(str);
+      str = "";
+    } catch (e) {
+      continue;
+    }
+
+    List<dynamic> toDeleteList = List.empty(growable: true);
+    for (var jse in js) {
+      if (jse is String) {
+        jse = jse.trim();
+        for (var k = 0; k < cntFileType; k++) {
+          if (jse != "" && jse.endsWith("${knowFileTypes[k]}.map") == false) continue;
+
+          toDeleteList.add(jse);
+        }
+      } else if (jse is Map) {
+        if (jse.containsKey('url') == false) continue;
+
+        for (var k = 0; k < cntFileType; k++) {
+          if (jse["url"]?.endsWith("${knowFileTypes[k]}.map") == false) continue;
+
+          toDeleteList.add(jse);
+        }
+      }
+    }
+
+    if (toDeleteList.isEmpty) continue;
+    for (var jse in toDeleteList) {
+      js.remove(jse);
+    }
+    toDeleteList.clear();
+
+    hasRemove = true;
+    str = json.encode(js);
+    if (isJsonNeedFix) {
+      str = str.replaceAllMapped(
+          RegExp(
+            r'([\{\,])"([a-zA-Z0-9]+)?":',
+            multiLine: true,
+          ), (m) {
+        return '${m.group(1)}${m.group(2)}:';
+      });
+    }
+    content = content.replaceAll(matches[i].group(0) ?? "[{}]", str);
+  }
+
+  if (hasRemove) {
+    f.writeAsStringSync(content.trim());
+    print("Service Worker Fixed: $path");
+  }
 }
