01d36d8113cabbd8942b19cc65c8c4f42d93752d
Leam Lidara <leamlidara@gmail.com>
2023-09-21 02:29:47 +0700

    block fix Service Worker

--- bin/sourcemap-remover.dart
+++ bin/sourcemap-remover.dart
@@ -64,11 +64,11 @@ void main(List<String> arguments) {
 void _fixSourceMapNotFound(String path) {
   var f = File(path);
   String content = f.readAsStringSync();
-  if (content.contains("# sourceMappingURL=") == false) return;
+  if (content.contains("sourceMappingURL=") == false) return;
 
   for (var i in [
-    RegExp(r"\/\/\# sourceMappingURL=[a-zA-Z0-9\.\-\~ ]+\.map"),
-    RegExp(r"\/\*\# sourceMappingURL=[a-zA-Z0-9\.\-\~ ]+\.map[ ]+?\*\/")
+    RegExp(r"\/\/\#[ ]+?sourceMappingURL=[a-zA-Z0-9\.\-\~ ]+\.map"),
+    RegExp(r"\/\*\#[ ]+?sourceMappingURL=[a-zA-Z0-9\.\-\~ ]+\.map[ ]+?\*\/")
   ]) {
     content = content.replaceAll(i, "");
   }
@@ -78,21 +78,24 @@ void _fixSourceMapNotFound(String path) {
 }
 
 void _fixServiceWorker(String path, List<String> knowFileTypes) {
+  print("Due to SDK failure, this is not working!");
+  return;
+
   var f = File(path);
-  RegExp regex = RegExp(r"\[(.)+?\]", dotAll: true, multiLine: true, unicode: true);
+
+  final regex = RegExp(r"\[(.)*?\]", dotAll: true, multiLine: true);
   String content = f.readAsStringSync();
-  List<RegExpMatch> matches = regex.allMatches(content).toList();
-  var cnt = matches.length;
-  if (cnt < 1) return;
+
+  var matches = regex.allMatches(content);
   print("Service Worker Found: $path");
-  print("Fixing...");
 
   bool hasRemove = false;
   int cntFileType = knowFileTypes.length;
-  for (var i = 0; i < cnt; i++) {
-    List<dynamic> js;
-    String str = matches[i].group(0) ?? "[]";
+
+  for (final match in matches) {
+    String str = match.group(0) ?? "{}";
     bool isJsonNeedFix = false;
+    List<dynamic> js;
     try {
       str = str.replaceAll(RegExp(r"^([\s\t])*", multiLine: true, unicode: true), "");
       str = str.replaceAllMapped(
@@ -105,6 +108,7 @@ void _fixServiceWorker(String path, List<String> knowFileTypes) {
       });
       str = str.replaceAll(RegExp(r"\},(\n|\r\n)?\]"), "}]");
       js = json.decode(str);
+      print(str);
       str = "";
     } catch (e) {
       continue;
@@ -119,9 +123,7 @@ void _fixServiceWorker(String path, List<String> knowFileTypes) {
 
           toDeleteList.add(jse);
         }
-      } else if (jse is Map) {
-        if (jse.containsKey('url') == false) continue;
-
+      } else if (jse is Map && jse.containsKey('url')) {
         for (var k = 0; k < cntFileType; k++) {
           if (jse["url"]?.endsWith("${knowFileTypes[k]}.map") == false) continue;
 
@@ -147,7 +149,7 @@ void _fixServiceWorker(String path, List<String> knowFileTypes) {
         return '${m.group(1)}${m.group(2)}:';
       });
     }
-    content = content.replaceAll(matches[i].group(0) ?? "[{}]", str);
+    content = content.replaceAll(match.group(0) ?? "", str);
   }
 
   if (hasRemove) {
